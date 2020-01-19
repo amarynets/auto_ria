e9536640afc1f1aedd78d33604a00b21116294da
Andrii Marynets <andrii.marynets@gmail.com>
2020-01-19 19:16:00 +0200

    Init

---
+++ auto_crawler/auto_crawler/spiders/auto_ria.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class AutoRiaSpider(scrapy.Spider):
+    name = 'auto_ria'
+    allowed_domains = ['auto.ria.com']
+    start_urls = ['https://auto.ria.com/car/bmw/']
+
+    def start_requests(self):
+        for i in range(1, 21):
+            yield scrapy.Request(
+                url=f'https://auto.ria.com/car/bmw/?page={i}&countpage=100',
+                callback=self.parse,
+                cookies={'ipp': 100}
+            )
+
+    def parse(self, response):
+        all_cars = response.css('section.ticket-item.new__ticket.t.paid')
+        for car in all_cars:
+            item = {
+                'itemLink': car.css('div.content-bar a.m-link-ticket::attr(href)').extract_first(),
+                'location': car.css('ul li.view-location::text').extract_first(default='').strip(),
+                'race': car.xpath('.//ul/li/i[@class="icon-mileage"]/../text()').extract_first(default='').strip(),
+                'fuelName': car.xpath('.//ul/li/i[@class="icon-fuel"]/../text()').extract_first(default='').strip(),
+                'gearboxName': car.xpath('.//ul/li/i[@class="icon-transmission"]/../text()').extract_first(default='').strip(),
+            }
+            yield scrapy.Request(
+                url=item['itemLink'],
+                callback=self.parse_car,
+                meta={'item': item}
+            )
+
+    def parse_car(self, response):
+        item = response.meta['item']
+        item['title'] = response.css('h1.head::attr(title)').extract_first()
+        item['usd'] = response.css('div.price_value strong::text').extract_first()
+        item['eur'] = response.css('span[data-currency="EUR"]::text').extract_first()
+        item['uah'] = response.css('span[data-currency="UAH"]::text').extract_first()
+        item['phone'] = response.css('div.phones_item span::attr(data-phone-number)').extract_first(default='')
+        item['description'] = response.css('div#full-description::text').extract_first()
+        item['color'] = response.xpath('.//span[@class="car-color"]/../text()').extract_first()
+        item['markName'] = response.css('h1.head span[itemprop="brand"]::text').extract_first()
+        item['modelName'] = response.css('h1.head span[itemprop="name"]::text').extract_first()
+        item['category'] = response.css('div#description_v3 dl dd::text').extract_first(default='').strip()
+        yield item
---
+++ auto_crawler/run.py
@@ -0,0 +1,3 @@
+from scrapy.cmdline import execute
+
+execute('scrapy crawl auto_ria -o cars.csv'.split())
