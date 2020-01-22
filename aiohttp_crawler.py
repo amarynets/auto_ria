d5161d58517a13b1b674af4679741cc85d295d5c
Andrii Marynets <andrii.marynets@gmail.com>
2020-01-22 23:42:35 +0200

    Add async crawler

---
+++ aiohttp_crawler.py
@@ -0,0 +1,44 @@
+import asyncio
+import csv
+import time
+import aiohttp
+
+from bs_parser import parse_car, parse_list
+
+
+async def fetch_page(url):
+    cookies = {'ipp': 100}
+    async with aiohttp.ClientSession(cookies=cookies) as session:
+        async with session.get(url) as resp:
+            return await resp.text()
+
+
+async def process(pages, size):
+    start_time = time.time()
+    fieldnames = ['itemLink', 'location', 'race', 'fuelName', 'gearboxName', 'title', 'usd', 'eur', 'uah', 'phone', 'description', 'color', 'markName', 'modelName', 'category']
+    list_pages_to_fetch = [fetch_page(f'https://auto.ria.com/car/bmw/?page={i}&countpage={size}') for i in range(1, pages + 1)]
+    list_pages = await asyncio.gather(*list_pages_to_fetch)
+
+    items_on_pages = [{'item': item, 'url': item['itemLink']} for page in list_pages for item in parse_list(page)]
+    items_on_pages = list(zip(*[items_on_pages[i::100] for i in range(100)]))
+    for items in items_on_pages:
+        list_car_pages_to_fetch = [fetch_page(i['url']) for i in items]
+        cars_pages = await asyncio.gather(*list_car_pages_to_fetch)
+
+        for item, car in zip(items, (parse_car(r) for r in cars_pages)):
+            item['item'].update(car)
+
+    with open('res.csv', 'w') as f:
+        csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
+        csv_writer.writeheader()
+        for items in items_on_pages:
+            for item in items:
+                csv_writer.writerow(item['item'])
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('Work is done. Elapsed time: ',  elapsed_time)
+
+
+asyncio.run(process(3, 100))
+
---
+++ bs_parser.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup as Bs
+
+
+def parse_list(response):
+    soup = Bs(response)
+    all_cars = soup.select('section.ticket-item.new__ticket.t')
+    for car in all_cars:
+        item = {
+            'itemLink': car.select_one('div.content-bar a.m-link-ticket').attrs['href'],
+            'location': car.select_one('ul li.view-location').text.strip(),
+            'race': car.select_one('ul li i.icon-mileage').parent.text.strip(),
+            'fuelName': car.select_one('ul li i.icon-fuel').parent.text.strip(),
+            'gearboxName': car.select_one('ul li i.icon-transmission').parent.text.strip(),
+        }
+        yield item
+
+
+def parse_car(response):
+    soup = Bs(response)
+    item = {
+        'title': soup.select_one('h1.head').attrs.get('title'),
+        'usd': soup.select_one('div.price_value strong'),
+        'eur': soup.select_one('span[data-currency="EUR"]'),
+        'uah': soup.select_one('span[data-currency="UAH"]'),
+        'phone': soup.select_one('div.phones_item span').attrs.get('data-phone-number'),
+        'description': soup.select_one('div#full-description'),
+        'color': '',
+        'markName': soup.select_one('h1.head span[itemprop="brand"]'),
+        'modelName': soup.select_one('h1.head span[itemprop="name"]'),
+        'category': soup.select_one('div#description_v3 dl dd'),
+    }
+
+    for k, v in item.items():
+        try:
+            item[k] = v.text if not isinstance(v, str) else v
+        except Exception as e:
+            item[k] = ''
+    color = soup.select_one('span.car-color')
+    if color:
+        item['color'] = color.parent.text
+
+    return item
