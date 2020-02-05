70ba72f89d081a60027bed016c444a1f715e4aff
Andrii Marynets <andrii.marynets@gmail.com>
2020-02-05 22:30:25 +0200

    Six

--- auto_crawler/run.py
+++ auto_crawler/run.py
@@ -1,3 +1,3 @@
 from scrapy.cmdline import execute
 
-execute('scrapy crawl auto_ria -a car=bmw -a pages=20 -a size=100 -o cars.csv'.split())
+execute('scrapy crawl auto_ria -a car=bmw -a pages=2 -a size=10 -o cars.csv'.split())
